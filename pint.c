0c038e91126ef80f66339121a2a2acde3054da12
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2023-12-15 02:21:20 +0000

    Update pint.c

--- pint.c
+++ pint.c
@@ -4,6 +4,8 @@
  * pint - Prints the value at the top of the stack, followed by a new line.
  * @stack: Double pointer to the beginning of the stack
  * @line_number: Line number in Monty byte code file
+ *
+ * Return: Nothing
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
