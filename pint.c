8b572ccdbfd5354f7c2b0b6692f2d3fd00ad23d9
markdrahg <neutral520@gmail.com>
2023-12-14 19:07:17 -0800

    TEST TRYER

--- pint.c
+++ pint.c
@@ -10,7 +10,6 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-		freer(*stack);
 		exit(EXIT_FAILURE);
 	}
 
