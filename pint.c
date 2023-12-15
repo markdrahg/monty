206a7c0587f9b09f421845e07423bf21b95a9ae0
markdrahg <neutral520@gmail.com>
2023-12-14 18:37:52 -0800

    PUSH TRY

--- pint.c
+++ pint.c
@@ -1,15 +1,6 @@
 #include "monty.h"
 
 /**
- * exit_with_failure - Print error message and exit with EXIT_FAILURE.
- */
-void exit_with_failure(void)
-{
-	fprintf(stderr, "Error: malloc failed\n");
-	exit(EXIT_FAILURE);
-}
-
-/**
  * pint - Prints the value at the top of the stack, followed by a new line.
  * @stack: Double pointer to the beginning of the stack
  * @line_number: Line number in Monty byte code file
@@ -19,9 +10,8 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-		exit_with_failure();
+		exit(EXIT_FAILURE);
 	}
 
 	printf("%d\n", (*stack)->n);
 }
-
